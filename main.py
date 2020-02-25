e100bbef5fd9e074a77de74a82248eb80bae0675
Pierre Schumacher <pier_schumacher@hotmail.com>
2020-02-25 14:56:57 +0100

    forgotfiles

---
+++ main.py
@@ -0,0 +1,21 @@
+from training_loop import main
+from omegaconf import OmegaConf
+import wandb
+from pudb import set_trace
+
+ant_env = False
+vrep = True
+show = False
+
+default_cnf = OmegaConf.load('configs/default_conf.yaml')
+if vrep:
+    cnf = OmegaConf.load('configs/vrep_conf.yaml')
+if ant_env:
+    cnf = OmegaConf.load('configs/ant_conf.yaml')
+# Parameters of second cnf file overwrite those of first
+cnf = OmegaConf.merge(default_cnf, cnf)
+cnf.merge_with_cli()
+OmegaConf.set_struct(cnf, True)
+
+wandb.init(project='research', entity=cnf.entity, config=cnf)
+main(cnf)

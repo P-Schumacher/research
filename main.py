b3114af2a6232ef4e5c65db9447bd38d38c274ca
Pierre Schumacher <pier_schumacher@hotmail.com>
2020-07-10 09:58:58 +0200

    sync

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import sys
 import os
 from pudb import set_trace
 #os.environ['CUDA_VISIBLE_DEVICES']='-1'
-ant_env = True 
-vrep = False
+ant_env = False
+vrep = True
 
 
 name = [sys.argv[1] if len(sys.argv) >= 2 else None][0]

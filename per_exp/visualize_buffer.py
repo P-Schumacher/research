ce26b9302848a9e32c89b24c1525fff41f30b1e9
Pierre Schumacher <pier_schumacher@hotmail.com>
2020-09-26 14:13:37 +0200

    forgot

--- per_exp/visualize_buffer.py
+++ per_exp/visualize_buffer.py
@@ -3,11 +3,10 @@ from pudb import set_trace
 from scipy import stats
 from matplotlib import pyplot as plt
 import matplotlib
-matplotlib.use('GTK3Agg')
-plt.style.use('seaborn')
-#plt.figure(figsize=(10, 4)),
+
+#matplotlib.use('GTK3Agg')
+plt.style.use(['seaborn', 'thesis'])
 plt.figure(figsize=(6, 5)),
-plt.subplot(121)
 m1 = np.load('./buffer_data/m1.npy')
 m2 = np.load('./buffer_data/m2.npy') 
 rew = np.load('./buffer_data/reward.npy')
@@ -36,40 +35,24 @@ positions = np.vstack([X.ravel(), Y.ravel()])
 values = np.vstack([m1, m2])
 kernel = stats.gaussian_kde(values)
 Z = np.reshape(kernel(positions).T, X.shape)
-
+print(np.rot90(Z).shape)
 plt.imshow(np.rot90(Z), cmap=plt.cm.gist_earth_r,
           extent=[xmin, xmax, ymin, ymax])
 #plt.plot(m1, m2, 'k.', markersize=4, marker='o')
 cb = plt.colorbar()
 cb.set_ticks([np.min(Z), (np.max(Z)+np.min(Z))/2, np.max(Z)])
 cb.set_ticklabels([0, 0.5, 1])
-#plt.xlim([xmin, xmax+1])
-#plt.ylim([ymin, ymax])
-plt.xlim([0, 1001])
+plt.xlim([xmin, xmax+1])
+plt.ylim([ymin, ymax+1])
 #plt.ylim([250, 350])
 plt.margins(x=0)
 #loc, labels = plt.xticks()
 #plt.xticks(ticks=loc, labels=np.arange(0, 140, 20))
 #loc, labels = plt.yticks()
 #plt.yticks(ticks=loc, labels=np.arange(0, 140, 20))
-plt.xlabel('Transition')
-plt.ylabel('High-level training iterations')
-plt.title('Transition density map')
-plt.savefig('map_n_points.pdf')
-plt.subplot(122)
-plt.plot(rew[:1000])
-new_rew = []
-for re in rew[:1000]:
-    if re == -1.:
-        new_rew.append(re)
-    else:
-        for i in range(1):
-            new_rew.append(re)
-plt.plot(new_rew)
-plt.xlabel('Transition')
-plt.ylabel('High-level reward')
-plt.xlim([0,1000])
-plt.legend(['Original distribution', 'Broadened distribution'])
-plt.title('Reward distribution in the buffer')
-plt.savefig('multiple_dones_big.pdf')
+plt.xlabel('transition number')
+plt.ylabel('high-level training iterations')
+plt.tight_layout()
+plt.savefig('unif_buff_vis.pdf')
 plt.show()
+
